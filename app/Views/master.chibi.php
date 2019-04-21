1c61498a149f516cb4b4d6b02a1190cb30e4d7cd
Inani El Houssain <inanielhoussain@gmail.com>
2019-04-21 22:17:19 +0100

    Welcome Page

--- app/Views/master.chibi.php
+++ app/Views/master.chibi.php
@@ -5,10 +5,34 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <title>TEST</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <title>Welcome</title>
 </head>
-<body>
-    <h1>Hello World</h1>
+<body class="bg-blue container mx-auto">
+    <div id="wrapper" class="flex h-screen items-center justify-center">
+            <p class="text-white text-2xl font-mono" id="text"></p>
+    </div>
+
+    <script>
+        typeString = ['Welcome, Feel free to use me :D'];
+        let  i = 0;
+        let count = 0;
+        let selectedText = '';
+        let text = '';
+        (function type() {
+            if (count === typeString.length) {
+                count = 0;
+                return;
+            }
+            selectedText = typeString[count];
+            text = selectedText.slice(0, ++i);
+            document.getElementById('text').innerHTML = text;
+            if (text.length === selectedText.length) {
+                count++;
+                return;
+            }
+            setTimeout(type, 100);
+        }());
+    </script>
 </body>
 </html>
